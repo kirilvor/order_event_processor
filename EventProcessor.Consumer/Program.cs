1eeddcf315cdfce7993efc7d92a95096ca756999
Kiril Vorotyntsev <xvorotyn@fi.muni.cz>
2025-05-19 23:31:49 +0200

    Other files

---
+++ EventProcessor.Consumer/Program.cs
@@ -0,0 +1,26 @@
+﻿using EventProcessor.Consumer;
+using EventProcessor.Consumer.Repositories;
+using Npgsql;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "rabbitmq", 
+    UserName = "username",
+    Password = "password"
+};
+
+// Create DB
+var connectionString = "Server=localhost;Port=5430;Userid=pgusername;Password=pgpassword;Database=ordersdb";
+var dbContext = new DbContext(connectionString);
+
+// Create Repositories
+var orderRepository = new OrderRepository(dbContext);
+
+// Create RabbitMQ consumer
+await using var consumer = new EventConsumer(orderRepository);
+
+await consumer.CreateConsumerAsync(factory);
+await consumer.StartConsumerAsync();
+
+while (true) ;
---
+++ EventProcessor.Producer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Text.Json;
+using EventProcessor.Shared.Models;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "rabbitmq", 
+    UserName = "username",
+    Password = "password"
+};
+
+await using var connection = await factory.CreateConnectionAsync();
+await using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "orderQueue", false, false, false);
+
+var propertiesOrder = new BasicProperties
+{
+    Headers = new Dictionary<string, object?>
+    {
+        { "X-MsgType", Encoding.UTF8.GetBytes("OrderEvent") }
+    }
+};
+
+var propertiesPayment = new BasicProperties
+{
+    Headers = new Dictionary<string, object?>
+    {
+        { "X-MsgType", Encoding.UTF8.GetBytes("PaymentEvent") }
+    }
+};
+    
+
+var testEvent = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(new OrderEvent()
+{
+    Id = "TEST_ID_3",
+    Product = "test",
+    Currency = "UAH",
+    Total = 1337.12m
+}));
+
+var testPayment = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(new PaymentEvent()
+{
+    OrderId = "TEST_ID_3",
+    Amount = 1337.12m
+}));
+await channel.BasicPublishAsync("", "orderQueue", true, propertiesOrder, testEvent);
+
+await channel.BasicPublishAsync("", "orderQueue", true, propertiesPayment, testPayment);
+Console.WriteLine($"[x] Sent");
